6bf79752dc54eedae8ac25d164d542c5dd36d39b
Aaron Jensen <ajensen@webmd.net>
2023-08-15 11:02:43 -0700

    Linting

--- Carbon.Registry/Functions/Set-CRegistryKeyValue.ps1
+++ Carbon.Registry/Functions/Set-CRegistryKeyValue.ps1
@@ -4,15 +4,22 @@ function Set-CRegistryKeyValue
     <#
     .SYNOPSIS
     Sets a value in a registry key.
-    
+
     .DESCRIPTION
-    The `Set-CRegistryKeyValue` function sets the value of a registry key. If the key doesn't exist, it is created first. Uses PowerShell's `New-ItemPropery` to create the value if doesn't exist. Otherwise uses `Set-ItemProperty` to set the value.
+    The `Set-CRegistryKeyValue` function sets the value of a registry key. If the key doesn't exist, it is created
+    first. Uses PowerShell's `New-ItemPropery` to create the value if doesn't exist. Otherwise uses `Set-ItemProperty`
+    to set the value.
 
-    `DWord` and `QWord` values are stored in the registry as unsigned integers. If you pass a negative integer for the `DWord` and `QWord` parameters, PowerShell will convert it to an unsigned integer before storing. You won't get the same negative number back.
+    `DWord` and `QWord` values are stored in the registry as unsigned integers. If you pass a negative integer for the
+    `DWord` and `QWord` parameters, PowerShell will convert it to an unsigned integer before storing. You won't get the
+    same negative number back.
 
-    To store integer values greater than `[Int32]::MaxValue` or `[Int64]::MaxValue`, use the `UDWord` and `UQWord` parameters, respectively, which are unsigned integers. These parameters were in Carbon 2.0.
+    To store integer values greater than `[Int32]::MaxValue` or `[Int64]::MaxValue`, use the `UDWord` and `UQWord`
+    parameters, respectively, which are unsigned integers. These parameters were in Carbon 2.0.
 
-    In versions of Carbon before 2.0, you'll need to convert these large unsigned integers into signed integers. You can't do this with casting. Casting preservers the value, not the bits underneath. You need to re-interpret the bits. Here's some sample code:
+    In versions of Carbon before 2.0, you'll need to convert these large unsigned integers into signed integers. You
+    can't do this with casting. Casting preservers the value, not the bits underneath. You need to re-interpret the
+    bits. Here's some sample code:
 
         # Carbon 1.0
         $bytes = [BitConverter]::GetBytes( $unsignedInt )
@@ -21,136 +28,127 @@ function Set-CRegistryKeyValue
 
         # Carbon 2.0
         Set-CRegistryKeyValue -Path $Path -Name 'MyUnsignedDWord' -UDWord $unsignedInt
-    
+
     .LINK
     Get-CRegistryKeyValue
-    
+
     .LINK
     Test-CRegistryKeyValue
-    
+
     .EXAMPLE
-    Set-CRegistryKeyValue -Path 'hklm:\Software\Carbon\Test -Name Status -String foobar 
-    
+    Set-CRegistryKeyValue -Path 'hklm:\Software\Carbon\Test -Name Status -String foobar
+
     Creates the `Status` string value under the `hklm:\Software\Carbon\Test` key and sets its value to `foobar`.
-    
+
     .EXAMPLE
     Set-CRegistryKeyValue -Path 'hklm:\Software\Carbon\Test -Name ComputerName -String '%ComputerName%' -Expand
-    
+
     Creates an expandable string.  When retrieving this value, environment variables will be expanded.
-    
+
     .EXAMPLE
     Set-CRegistryKeyValue -Path 'hklm:\Software\Carbon\Test -Name Movies -String ('Signs','Star Wars','Raiders of the Lost Ark')
-    
+
     Sets a multi-string (i.e. array) value.
-    
+
     .EXAMPLE
-    Set-CRegistryKeyValue -Path hklm:\Software\Carbon\Test -Name 'SomeBytes' -Binary ([byte[]]@( 1, 2, 3, 4)) 
-    
+    Set-CRegistryKeyValue -Path hklm:\Software\Carbon\Test -Name 'SomeBytes' -Binary ([byte[]]@( 1, 2, 3, 4))
+
     Sets a binary value (i.e. `REG_BINARY`).
-    
+
     .EXAMPLE
     Set-CRegistryKeyValue -Path hklm:\Software\Carbon\Test -Name 'AnInt' -DWord 48043
-    
+
     Sets a binary value (i.e. `REG_DWORD`).
-    
+
     .EXAMPLE
     Set-CRegistryKeyValue -Path hklm:\Software\Carbon\Test -Name 'AnInt64' -QWord 9223372036854775807
-    
+
     Sets a binary value (i.e. `REG_QWORD`).
-    
+
     .EXAMPLE
     Set-CRegistryKeyValue -Path hklm:\Software\Carbon\Test -Name 'AnUnsignedInt' -UDWord [uint32]::MaxValue
-    
+
     Demonstrates how to set a registry value with an unsigned integer or an integer bigger than `[int]::MaxValue`.
 
-    The `UDWord` parameter was added in Carbon 2.0. In earlier versions of Carbon, you have to convert the unsigned int's bits to a signed integer:
+    The `UDWord` parameter was added in Carbon 2.0. In earlier versions of Carbon, you have to convert the unsigned
+    int's bits to a signed integer:
 
         $bytes = [BitConverter]::GetBytes( $unsignedInt )
         $signedInt = [BitConverter]::ToInt32( $bytes, 0 )
         Set-CRegistryKeyValue -Path $Path -Name 'MyUnsignedDWord' -DWord $signedInt
-        
+
     .EXAMPLE
     Set-CRegistryKeyValue -Path hklm:\Software\Carbon\Test -Name 'AnUnsignedInt64' -UQWord [uint64]::MaxValue
-    
-    Demonstrates how to set a registry value with an unsigned 64-bit integer or a 64-bit integer bigger than `[long]::MaxValue`.
 
-    The `UQWord parameter was added in Carbon 2.0. In earlier versions of Carbon, you have to convert the unsigned int's bits to a signed integer:
+    Demonstrates how to set a registry value with an unsigned 64-bit integer or a 64-bit integer bigger than
+    `[long]::MaxValue`.
+
+    The `UQWord parameter was added in Carbon 2.0. In earlier versions of Carbon, you have to convert the unsigned int's
+    bits to a signed integer:
 
         $bytes = [BitConverter]::GetBytes( $unsignedInt )
         $signedInt = [BitConverter]::ToInt64( $bytes, 0 )
         Set-CRegistryKeyValue -Path $Path -Name 'MyUnsignedDWord' -DWord $signedInt
-    
+
     .EXAMPLE
     Set-CRegistryKeyValue -Path hklm:\Software\Carbon\Test -Name 'UsedToBeAStringNowShouldBeDWord' -DWord 1 -Force
-    
-    Uses the `Force` parameter to delete the existing `UsedToBeAStringNowShouldBeDWord` before re-creating it.  This flag is useful if you need to change the type of a registry value.
+
+    Uses the `Force` parameter to delete the existing `UsedToBeAStringNowShouldBeDWord` before re-creating it.  This
+    flag is useful if you need to change the type of a registry value.
     #>
-    [CmdletBinding(SupportsShouldPRocess=$true,DefaultParameterSetName='String')]
+    [CmdletBinding(SupportsShouldPRocess, DefaultParameterSetName='String')]
     param(
-        [Parameter(Mandatory=$true)]
-        [string]
         # The path to the registry key where the value should be set.  Will be created if it doesn't exist.
-        $Path,
-        
-        [Parameter(Mandatory=$true)]
-        [string]
+        [Parameter(Mandatory)]
+        [string] $Path,
+
         # The name of the value being set.
-        $Name,
-        
-        [Parameter(Mandatory=$true,ParameterSetName='String')]
+        [Parameter(Mandatory)]
+        [string] $Name,
+
+        # The value's data.  Creates a value for holding string data (i.e. `REG_SZ`). If `$null`, the value will be saved as an empty string.
+        [Parameter(Mandatory, ParameterSetName='String')]
         [AllowEmptyString()]
         [AllowNull()]
-        [string]
-        # The value's data.  Creates a value for holding string data (i.e. `REG_SZ`). If `$null`, the value will be saved as an empty string.
-        $String,
-        
-        [Parameter(ParameterSetName='String')]
-        [Switch]
-        # The string should be expanded when retrieved.  Creates a value for holding expanded string data (i.e. `REG_EXPAND_SZ`).
-        $Expand,
-        
-        [Parameter(Mandatory=$true,ParameterSetName='Binary')]
-        [byte[]]
-        # The value's data.  Creates a value for holding binary data (i.e. `REG_BINARY`).
-        $Binary,
-        
-        [Parameter(Mandatory=$true,ParameterSetName='DWord')]
-        [int]
-        # The value's data.  Creates a value for holding a 32-bit integer (i.e. `REG_DWORD`).
-        $DWord,
-        
-        [Parameter(Mandatory=$true,ParameterSetName='DWordAsUnsignedInt')]
-        [uint32]
-        # The value's data as an unsigned integer (i.e. `UInt32`).  Creates a value for holding a 32-bit integer (i.e. `REG_DWORD`).
-        $UDWord,
-        
-        [Parameter(Mandatory=$true,ParameterSetName='QWord')]
-        [long]
-        # The value's data.  Creates a value for holding a 64-bit integer (i.e. `REG_QWORD`).
-        $QWord,
-        
-        [Parameter(Mandatory=$true,ParameterSetName='QWordAsUnsignedInt')]
-        [uint64]
-        # The value's data as an unsigned long (i.e. `UInt64`).  Creates a value for holding a 64-bit integer (i.e. `REG_QWORD`).
-        $UQWord,
-        
-        [Parameter(Mandatory=$true,ParameterSetName='MultiString')]
-        [string[]]
-        # The value's data.  Creates a value for holding an array of strings (i.e. `REG_MULTI_SZ`).
-        $Strings,
-        
-        [Switch]
-        # Removes and re-creates the value.  Useful for changing a value's type.
-        $Force,
-        
-        [Parameter(DontShow=$true)]
-        [Switch]
-        # OBSOLETE. Will be removed in a future version of Carbon.
-        $Quiet
-    )
-    
-    Set-StrictMode -Version 'Latest'
+        [string] $String,
 
+        # The string should be expanded when retrieved.  Creates a value for holding expanded string data (i.e. `REG_EXPAND_SZ`).
+        [Parameter(ParameterSetName='String')]
+        [Switch] $Expand,
+
+        # The value's data.  Creates a value for holding binary data (i.e. `REG_BINARY`).
+        [Parameter(Mandatory, ParameterSetName='Binary')]
+        [byte[]] $Binary,
+
+        # The value's data.  Creates a value for holding a 32-bit integer (i.e. `REG_DWORD`).
+        [Parameter(Mandatory, ParameterSetName='DWord')]
+        [int] $DWord,
+
+        # The value's data as an unsigned integer (i.e. `UInt32`).  Creates a value for holding a 32-bit integer (i.e. `REG_DWORD`).
+        [Parameter(Mandatory, ParameterSetName='DWordAsUnsignedInt')]
+        [uint32] $UDWord,
+
+        # The value's data.  Creates a value for holding a 64-bit integer (i.e. `REG_QWORD`).
+        [Parameter(Mandatory, ParameterSetName='QWord')]
+        [long] $QWord,
+
+        # The value's data as an unsigned long (i.e. `UInt64`).  Creates a value for holding a 64-bit integer (i.e. `REG_QWORD`).
+        [Parameter(Mandatory, ParameterSetName='QWordAsUnsignedInt')]
+        [uint64] $UQWord,
+
+        # The value's data.  Creates a value for holding an array of strings (i.e. `REG_MULTI_SZ`).
+        [Parameter(Mandatory, ParameterSetName='MultiString')]
+        [string[]] $Strings,
+
+        # Removes and re-creates the value.  Useful for changing a value's type.
+        [Switch] $Force,
+
+        # OBSOLETE. Will be removed in a future version of Carbon.
+        [Parameter(DontShow=$true)]
+        [Switch] $Quiet
+    )
+
+    Set-StrictMode -Version 'Latest'
     Use-CallerPreference -Cmdlet $PSCmdlet -Session $ExecutionContext.SessionState
 
     if( $PSBoundParameters.ContainsKey('Quiet') )
@@ -160,11 +158,11 @@ function Set-CRegistryKeyValue
 
     $value = $null
     $type = $pscmdlet.ParameterSetName
-    switch -Exact ( $pscmdlet.ParameterSetName )
+    switch -Exact ($PSCmdlet.ParameterSetName)
     {
-        'String' 
-        { 
-            $value = $String 
+        'String'
+        {
+            $value = $String
             if( $Expand )
             {
                 $type = 'ExpandString'
@@ -173,27 +171,27 @@ function Set-CRegistryKeyValue
         'Binary' { $value = $Binary }
         'DWord' { $value = $DWord }
         'QWord' { $value = $QWord }
-        'DWordAsUnsignedInt' 
-        { 
-            $value = $UDWord 
+        'DWordAsUnsignedInt'
+        {
+            $value = $UDWord
             $type = 'DWord'
         }
-        'QWordAsUnsignedInt' 
-        { 
-            $value = $UQWord 
+        'QWordAsUnsignedInt'
+        {
+            $value = $UQWord
             $type = 'QWord'
         }
         'MultiString' { $value = $Strings }
     }
-    
+
     Install-CRegistryKey -Path $Path
-    
-    if( $Force )
+
+    if ($Force)
     {
         Remove-CRegistryKeyValue -Path $Path -Name $Name
     }
 
-    if( Test-CRegistryKeyValue -Path $Path -Name $Name )
+    if (Test-CRegistryKeyValue -Path $Path -Name $Name)
     {
         $currentValue = Get-CRegistryKeyValue -Path $Path -Name $Name
         if( $currentValue -ne $value )
